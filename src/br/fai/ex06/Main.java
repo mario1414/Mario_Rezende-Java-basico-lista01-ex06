9cd36e8c6f7a22eecf34b0ea25d3c8e463babc03
mario <mariodentuo22@gmail.com>
2022-02-22 21:55:36 -0300

    FEAT:Criação do projeto

---
+++ src/br/fai/ex06/Main.java
@@ -0,0 +1,23 @@
+package br.fai.ex06;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Main app = new Main();
+		app.start();
+	}
+
+	private void start() {
+		
+		String[] carros = { "Civic", "Gol", "Palio", "Uno",};
+		
+		for (int i = 0; i < carros.length; i++) {
+			
+			String nomeDoCarro = carros[i];
+			System.out.println("Nome do carro: " + nomeDoCarro);
+		}
+		
+	}
+
+}
