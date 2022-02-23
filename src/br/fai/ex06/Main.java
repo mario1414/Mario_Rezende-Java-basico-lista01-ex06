49fbf1524fe25e9ce8e47195599fc067b6fecf8a
mario <mariodentuo22@gmail.com>
2022-02-22 22:23:00 -0300

    Feat: finalização

--- src/br/fai/ex06/Main.java
+++ src/br/fai/ex06/Main.java
@@ -9,12 +9,12 @@ public class Main {
 	}
 
 	private void start() {
-		
-		String[] carros = { "Civic", "Gol", "Palio", "Uno",};
-		
+
+		String[] carros = { "Civic", "Gol", "Palio", "Uno", };
+
 		try {
 			for (int i = 0; i < carros.length; i++) {
-				
+
 				String nomeDoCarro = carros[i];
 				System.out.println("Nome do carro: " + nomeDoCarro);
 			}
@@ -24,22 +24,35 @@ public class Main {
 		} finally {
 			System.out.println("este código será executado independende se der erro ou nao");
 		}
-		
+
 		for (int i = 0; i < carros.length; i++) {
-			
+
 			String nomeDoCarro = carros[i];
 			System.out.println("Nome do carro: " + nomeDoCarro);
 		}
-		
+
 		carros[1] = "L200";
-		
+
 		for (String carro : carros) {
 			System.out.println("Nome do carro: " + carro);
-			
+
 		}
 		
+		int tamanhoDoArray = carros.length + 1;
+		String[] novoArrayDeCarros = new String[tamanhoDoArray];
 		
+		for (int i = 0; i < carros.length; i++) {
+			
+			String carro = carros[i];
+			novoArrayDeCarros[i] = carro;
+		}
 		
+		novoArrayDeCarros[tamanhoDoArray - 1] = "Gol";
+		
+		for (String carro : novoArrayDeCarros) {
+			System.out.println("Novo array de carros: " + carro);
+			
+		}
 	}
 
 }
